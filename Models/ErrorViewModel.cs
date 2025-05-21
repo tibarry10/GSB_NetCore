e7fdb76dc0d73e7b7bb02ccdee1d6f0fadb39b42
Thierno Barry <tibarry10@outlook.com>
2025-05-21 15:44:17 +0200

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GSB_NetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
